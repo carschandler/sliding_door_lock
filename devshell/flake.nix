9236650ca17d8449fc37033146dad95a907d81e0
Cars Chandler <carschandler7@gmail.com>
2024-08-16 06:28:55 -0500

    Ready to print(?)

---
+++ devshell/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "CAD devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell { buildInputs = [ pkgs.freecad ]; };
+    };
+}
